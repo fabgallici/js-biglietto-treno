a5c89fb01da9f05166d2652c9acaa68d28a5a06d
cabal82 <fabriziogallici@gmail.com>
2019-10-17 17:39:40 +0200

    testo esercizio

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// Calcolo prezzo del treno:
+// Il programma dovrà chiedere
+// all’utente il numero di chilometri e
+// l’età e dovrà calcolare il prezzo totale
+// del viaggio.
+// Il prezzo del biglietto è definito in
+// base ai km (0.21 €cent al km) ma c’è
+// uno sconto del 20% per i minorenni e
+// del 40% per gli over 65
+
+
 //variabili iniziali
 var ticketPrice = 0.21;
 var youngDiscount = 20;
