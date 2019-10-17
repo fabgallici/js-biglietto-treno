833e5b186445382fefd1c2b5adba96bf0a7f867b
cabal82 <fabriziogallici@gmail.com>
2019-10-17 15:03:29 +0200

    calcolo prezzo scontato e test

--- main.js
+++ main.js
@@ -2,9 +2,22 @@
 var ticketPrice = 0.21;
 var youngDiscount = 20;
 var seniorDiscount = 40;
+var finalRoutePrice;
 //Richieste numero chilometri ed età utente
 var userNumKm = parseInt(prompt("Quanti km è lungo il viaggio?"));
 var userAge = parseInt(prompt("Quanti anni hai?"));
 
-var routePrice = userNumKm * ticketPrice;
-console.log(routePrice);
+var baseRoutePrice = userNumKm * ticketPrice;
+console.log(baseRoutePrice);
+
+// stabilire sconto in base all età,
+// minorenni 20% , over 65 anni 40%
+if (userAge < 18 ) {
+  finalRoutePrice = (baseRoutePrice / 100) * (100 - youngDiscount);
+} else if (userAge > 65) {
+  finalRoutePrice = (baseRoutePrice / 100) * (100 - seniorDiscount);
+} else  {
+  finalRoutePrice = baseRoutePrice;
+}
+
+console.log(finalRoutePrice);
