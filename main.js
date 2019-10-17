e5f19ae4f63c975d23ea14800d313635ca67fdd9
cabal82 <fabriziogallici@gmail.com>
2019-10-17 15:55:37 +0200

    visualizzazione scritta eventuale sconto applicato

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var ticketPrice = 0.21;
 var youngDiscount = 20;
 var seniorDiscount = 40;
 var finalRoutePrice;
+var youngUser, seniorUser;
+
 //Richieste numero chilometri ed età utente
 var userNumKm = parseInt(prompt("Quanti km è lungo il viaggio?"));
 var userAge = parseInt(prompt("Quanti anni hai?"));
@@ -14,12 +16,23 @@ var baseRoutePrice = userNumKm * ticketPrice;
 // minorenni 20% , over 65 anni 40%
 if (userAge < 18 ) {
   finalRoutePrice = (baseRoutePrice / 100) * (100 - youngDiscount);
+  youngUser = true;
 } else if (userAge > 65) {
   finalRoutePrice = (baseRoutePrice / 100) * (100 - seniorDiscount);
+  seniorUser = true;
 } else  {
   finalRoutePrice = baseRoutePrice;
 }
 // console.log(finalRoutePrice);
+
+//visualizzazione scritta eventuale sconto applicato
+if (youngUser === true) {
+  document.getElementById('discount-value').innerHTML = "Hai diritto ad uno sconto del: " + youngDiscount + "%";
+} else if (seniorUser === true) {
+  document.getElementById('discount-value').innerHTML = "Hai diritto ad uno sconto del: " + seniorDiscount + "%";
+} else {
+  document.getElementById('discount-value').innerHTML = "Non hai diritto ad alcuno sconto";
+}
 
 //visualizzazione prezzo finale sulla pagina
 document.getElementById('price-title').innerHTML = "Il prezzo del biglietto è di:";
